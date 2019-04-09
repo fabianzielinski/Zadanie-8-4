d93aa6d51d8856f82be3ca2289d38dace7e9477b
fabianzielinski <fabz@o2.pl>
2019-04-09 13:20:24 +0200

    Add button NewGame.

--- js/script.js
+++ js/script.js
@@ -1,17 +1,25 @@
 'use strict';
 
+// Var
+
 var compChoice;
 var playerChoice;
 
-var playerResult = 0;
-var compResult = 0;
-var gameResult = 0;
-var rounds = 0; 
+var playerResult;
+var compResult ;
+var gameResult;
+var rounds; 
+
+var output = document.getElementById('output');
+
+// Buttons
 
 var btnPaper = document.getElementById('paper');
 var btnRock = document.getElementById('rock');
 var btnScissors = document.getElementById('scissors');
-var output = document.getElementById('output');
+var newGame = document.getElementById('newGame');
+
+// Function
 
 var compMove =  function() {
 
@@ -25,7 +33,7 @@ var compMove =  function() {
 	} 
 	else {
 		compChoice = 'scissors';
-	};
+	}
 	return compChoice;
 }
 
@@ -33,15 +41,8 @@ var compMove =  function() {
 var playerMove = function(pmv) {
 
 	playerChoice = pmv;
-
 	compChoice = compMove();
-
-	// console.log(pmv);
-
-	// document.getElementById('output').innerHTML = playerChoice +'  '+ compChoice ;
-
 	game();
-
 }
 
 function game() {
@@ -52,22 +53,25 @@ function game() {
         return;
     } else if(compResult == (rounds-1)){
         document.getElementById('gameResult').innerHTML = "<span style=\"color:red\">" + 'YOU LOST THE ENTIRE GAME' + '<br>' + 'Game over, please press the New Game button' + "</span>";
-        document.getElementById('compResult').innerHTML = rounds;
+        document.getElementById('compResult').innerHTML =  rounds;
         return;
     } else {
     
         if(compChoice == playerChoice) {
             output.innerHTML = 'YOU TIED!';
-        } else if ((playerChoice == 'paper' && compChoice == 'rock') || (playerChoice == 'rock' && compChoice == 'scissors') || (playerChoice == 'scissors' && compChoice == 'paper')){
+        } 
+        else if ((playerChoice == 'paper' && compChoice == 'rock') || (playerChoice == 'rock' && compChoice == 'scissors') || (playerChoice == 'scissors' && compChoice == 'paper')) {
             output.innerHTML = 'YOU LOST with: ' + playerChoice;
-            document.getElementById('compResult').innerHTML = 'Komputer : ' + compResult++;
-        } else {
+            document.getElementById('compResult').innerHTML = compResult++;
+        } 
+        else {
             output.innerHTML = 'YOU WON with: ' + playerChoice;
-            document.getElementById('playerResult').innerHTML = 'Player : ' + playerResult++;
+            document.getElementById('playerResult').innerHTML = playerResult++;
         }
-    }
-}
+    };
+};
 
+//Event
 
 btnPaper.addEventListener('click', function(){
 	playerMove('paper');
@@ -79,4 +83,16 @@ btnRock.addEventListener('click', function(){
 
 btnScissors.addEventListener('click', function(){
 	playerMove ('scissors');
+});
+
+newGame.addEventListener('click', function(){
+    rounds = window.prompt('How many rounds = win?');
+    playerResult = document.getElementById('playerResult').innerHTML = 0;
+    compResult = document.getElementById('compResult').innerHTML = 0;
+    document.getElementById('gameResult').innerHTML = '';
+    document.getElementById('output').innerHTML = '';
+   
+    if(isNaN(rounds)){
+        output.innerHTML = 'Please pick a number!';
+    }
 });
